605df477a0b97670f68976df011b6efccdd0a655
klemen1999 <klemenskrlj8@gmail.com>
2021-04-16 12:22:28 -0700

    neznam importat classa v drugi file

--- src/navigation/scripts/auto_goals.py
+++ src/navigation/scripts/auto_goals.py
@@ -27,61 +27,69 @@ class Map:
         return mapT
 
 
-def get_goals():
-    path = os.path.dirname(os.path.realpath(__file__))
-    parent = os.path.abspath(os.path.join(path, os.pardir))
-    extension = "/images/map.pgm"
-    imagePath = parent+extension
-
-    img = cv2.imread(imagePath, 0)
-
-    image = np.copy(img) # map with 0 and 255 only
-    image[image < 240] = 0
-    image[image >= 240] = 255
-
-    skeleton = skeletonize(image, method="lee") # skeletonized map
-    skeleton[skeleton == 255] = 60
-    path = np.argwhere(skeleton == 60) # pixels that are part of the path
-
-    gridPoints = [] # grid inside the map where distance between is 25
-    for i in range(0, img.shape[0], 25):
-        for j in range(0, img.shape[1], 25):
-            if (i == 0 or i == img.shape[0] - 1
-                    or j == 0 or j == img.shape[1] - 1):
-                continue
-
-            if not (img[i + 1, j] == 205 and img[i - 1, j] == 205
-                    and img[i, j + 1] == 205 and img[i, j - 1] == 205):
-                gridPoints.append((i, j))
-
-    pathPoints = [] # calculating nearest path points for every grid points
-    for point in gridPoints:
-        maxD = np.inf
-        maxP = [0, 0]
-        for pathP in path:
-            dist = np.linalg.norm(point - pathP)
-            if dist < maxD:
-                maxD = dist
-                maxP = pathP
-        pathPoints.append((maxP[0],maxP[1]))
-
-    return pathPoints
-
-def transform_points(points, map):
-    transPoints = []
-    for point in points:
-        pt = PoseStamped()
-        pt_t = PoseStamped()
-        pt.pose.position.x = point[1] * map.res
-        
-        pt.pose.position.y = (map.height - point[0]) * map.res
-
-        pt_t = tf2_geometry_msgs.do_transform_pose(pt,map.transform)
-
-        transPoints.append(pt_t)
+class AutoNav:
+    def __init__(self, gridSize):
+        self.gridSize = gridSize
+        mapData = rospy.wait_for_message("map", OccupancyGrid)
+        self.map = Map(mapData)
 
 
-    return transPoints
+    def get_goalsImage(self):
+        path = os.path.dirname(os.path.realpath(__file__))
+        parent = os.path.abspath(os.path.join(path, os.pardir))
+        extension = "/images/map.pgm"
+        imagePath = parent + extension
+
+        img = cv2.imread(imagePath, 0)
+
+        image = np.copy(img)  # map with 0 and 255 only
+        image[image < 240] = 0
+        image[image >= 240] = 255
+
+        skeleton = skeletonize(image, method="lee")  # skeletonized map
+        skeleton[skeleton == 255] = 60
+        path = np.argwhere(skeleton == 60)  # pixels that are part of the path
+
+        gridPoints = []  # grid inside the map where distance between is self.gridSize
+        for i in range(0, img.shape[0], self.gridSize):
+            for j in range(0, img.shape[1], self.gridSize):
+                if (i == 0 or i == img.shape[0] - 1
+                        or j == 0 or j == img.shape[1] - 1):
+                    continue
+
+                if not (img[i + 1, j] == 205 and img[i - 1, j] == 205
+                        and img[i, j + 1] == 205 and img[i, j - 1] == 205):
+                    gridPoints.append((i, j))
+
+        pathPoints = []  # calculating nearest path points for every grid points
+        for point in gridPoints:
+            maxD = np.inf
+            maxP = [0, 0]
+            for pathP in path:
+                dist = np.linalg.norm(point - pathP)
+                if dist < maxD:
+                    maxD = dist
+                    maxP = pathP
+            pathPoints.append((maxP[0], maxP[1]))
+
+        return pathPoints
+
+    def transform_points(self, points):
+        transPoints = []
+        for point in points:
+            pt = PoseStamped()
+            pt.pose.position.x = point[1] * self.map.res
+            pt.pose.position.y = (self.map.height - point[0]) * self.map.res
+            pt_t = tf2_geometry_msgs.do_transform_pose(pt, self.map.transform)
+            transPoints.append((pt_t.pose, 360, 1))
+
+        return transPoints
+
+
+    def get_mapGoals(self):
+        points = self.get_goalsImage()
+        transPoints = self.transform_points(points)
+        return transPoints
 
 
 def move_points(points):
@@ -89,9 +97,11 @@ def move_points(points):
     rospy.loginfo("Waiting for move base server")
     client.wait_for_server()
 
-    for point in points:
-        move(point, client)
-
+    for (point, _, _) in points:
+        p = PoseStamped()
+        p.pose = point
+        move(p, client)
+        print("goig to next goal")
     rospy.loginfo("End")
 
 
@@ -101,19 +111,12 @@ def move(point, client):
     goal.target_pose.pose = point.pose
     goal.target_pose.pose.orientation.w = 1
     goal.target_pose.header.stamp = rospy.Time.now()
-    print(goal)
     client.send_goal(goal)
     client.wait_for_result()
 
 
 if __name__ == '__main__':
     rospy.init_node("goals_node")
-    mapData = rospy.wait_for_message("map", OccupancyGrid)
-
-    map = Map(mapData)
-
-    points = get_goals()
-
-    transPoints = transform_points(points,map)
-
-    move_points(transPoints)
+    navigator = AutoNav(25)
+    points = navigator.get_mapGoals()
+    move_points(points)
